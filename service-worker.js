8d9bad214ed3e70c973010470232a9f2f6d262d4
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-07 22:26:16 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,40 @@
+const CACHE = 'portfolio-v2';
+const FILES = [
+'./',
+'./index.html',
+'./projects.json',
+'./assets/css/style.css',
+'./assets/js/app.js'
+];
 
+
+self.addEventListener('install', e => {
+e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+
+self.addEventListener('fetch', e => {
+e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
+```javascript
+const CACHE_NAME = 'portfolio-cache-v1';
+const ASSETS = [
+'./',
+'./index.html',
+'./assets/css/style.css',
+'./assets/js/app.js'
+];
+
+
+self.addEventListener('install', e => {
+e.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+);
+});
+
+
+self.addEventListener('fetch', e => {
+e.respondWith(
+caches.match(e.request).then(res => res || fetch(e.request))
+);
+});

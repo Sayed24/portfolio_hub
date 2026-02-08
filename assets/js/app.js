406f62735973f6166dbf65eff27b6a0cb27ecbd9
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-07 22:19:09 -0800

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,28 @@
 
+const toggle = document.getElementById('themeToggle');
+const currentTheme = localStorage.getItem('theme');
+
+
+if (currentTheme) {
+document.documentElement.setAttribute('data-theme', currentTheme);
+toggle.textContent = currentTheme === 'dark' ? '☀️' : '🌙';
+}
+
+
+toggle.addEventListener('click', () => {
+let theme = document.documentElement.getAttribute('data-theme');
+if (theme === 'dark') {
+document.documentElement.removeAttribute('data-theme');
+localStorage.setItem('theme', 'light');
+toggle.textContent = '🌙';
+} else {
+document.documentElement.setAttribute('data-theme', 'dark');
+localStorage.setItem('theme', 'dark');
+toggle.textContent = '☀️';
+}
+});
+
+
+if ('serviceWorker' in navigator) {
+navigator.serviceWorker.register('service-worker.js');
+}

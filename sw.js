dd1cc99eef9b8f9bc7c639684e5d3fe3c0e69dc5
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-11 19:02:41 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("portfolio-cache").then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/styles.css",
+        "/app.js",
+        "/projects.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
 

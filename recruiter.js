907c47e02ebb09d32732751b2a9b1a1250ddf3b7
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-15 09:07:25 -0800

    Implement recruiter mode banner display
    Add a banner for recruiter mode when URL parameter is true.

--- recruiter.js
+++ recruiter.js
@@ -1,1 +1,13 @@
+(function(){
+const params=new URLSearchParams(location.search);
+if(params.get("recruiter")!=="true") return;
+
+const banner=document.createElement("div");
+banner.style.background="#111";
+banner.style.color="#fff";
+banner.style.padding="12px";
+banner.style.textAlign="center";
+banner.innerHTML="⭐ Recruiter Mode Enabled";
+document.body.prepend(banner);
+})();
 

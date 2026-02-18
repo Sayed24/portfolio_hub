e042bfb722dffeabc78944658e24cfc36e6f2033
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-18 11:06:10 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
+const CACHE="portfolio-v6";
+
 self.addEventListener("install",e=>{
  e.waitUntil(
-  caches.open("faang-v5").then(c=>c.addAll([
+  caches.open(CACHE).then(c=>c.addAll([
    "./",
-   "index.html",
-   "styles.css",
-   "app.js",
-   "projects.json"
+   "./index.html",
+   "./styles.css",
+   "./app.js"
   ]))
  );
 });

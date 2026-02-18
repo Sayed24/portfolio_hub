2dbe8cf8378acdd95bfb977bc35ce682b38bb330
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-18 11:05:12 -0800

    Update recruiter.js

--- recruiter.js
+++ recruiter.js
@@ -1,28 +1,7 @@
-function recruiterMode(){
+const rBtn=document.getElementById("recruiterToggle");
 
-let stats = JSON.parse(localStorage.analytics || "{}");
-
-const sorted = [...PROJECTS].sort((a,b)=>
- (stats[b.title]||0)-(stats[a.title]||0)
-);
-
-renderRecruiter(sorted);
-}
-
-function renderRecruiter(list){
-
-let html=`<h2 class="page-title">Recruiter View</h2>
-<section class="grid">`;
-
-list.forEach(p=>{
- html+=`
- <div class="card">
-   <img src="${p.image}">
-   <h3>${p.title}</h3>
- </div>`;
-});
-
-html+=`</section>`;
-
-document.getElementById("app").innerHTML=html;
+if(rBtn){
+ rBtn.onclick=()=>{
+   document.body.classList.toggle("recruiter");
+ };
 }

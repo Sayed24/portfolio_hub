0861ca1cdd4c91cbebd1e28b03378b9d2da780fb
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-18 11:13:00 -0800

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -1,14 +1,3 @@
-
-function generatePortfolioPDF(){
- const content=[...document.querySelectorAll(".card")]
-  .map(c=>c.innerText)
-  .join("\n\n");
-
- const blob=new Blob([content],{type:"text/plain"});
- const url=URL.createObjectURL(blob);
-
- const a=document.createElement("a");
- a.href=url;
- a.download="portfolio.txt";
- a.click();
+function downloadResume(){
+ window.open("resume.pdf");
 }

aae63e1f9093d13d63e9acf1981063f935819ea8
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-20 11:58:51 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+async function loadProjects() {
+  const res = await fetch("./data/projects.json");
+  const projects = await res.json();
 
+  const container = document.querySelector(".projects-grid");
+
+  projects.forEach(project => {
+    const card = document.createElement("div");
+    card.className = "card apple-card";
+
+    card.innerHTML = `
+      <img src="${project.image}" />
+      <div class="card-content">
+        <h3>${project.title}</h3>
+        <p>${project.description}</p>
+      </div>
+    `;
+
+    card.addEventListener("click", () => {
+      window.open(project.url, "_blank");
+    });
+
+    container.appendChild(card);
+  });
+}
+
+loadProjects();

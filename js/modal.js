4980ad076a54cf5330894e6005186abb14120e3b
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-20 12:09:15 -0800

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,20 @@
 
+const modal = document.getElementById('projectModal');
+
+window.openModal = function(project) {
+  modal.classList.remove('hidden');
+
+  modal.innerHTML = `
+    <div class="modal-content">
+      <h2>${project.title}</h2>
+      <img src="${project.image}">
+      <p>${project.description}</p>
+      <a href="${project.url}" target="_blank">Visit Project</a>
+      <button onclick="closeModal()">Close</button>
+    </div>
+  `;
+}
+
+window.closeModal = function() {
+  modal.classList.add('hidden');
+}

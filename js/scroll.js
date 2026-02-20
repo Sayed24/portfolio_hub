107d697a947afdd7fbed27b29f688003349911ec
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-20 12:03:43 -0800

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,1 +1,12 @@
 
+const reveals = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+});
+
+reveals.forEach(r => observer.observe(r));

68716455cfc0fac2f7fb4e2c506c442a6472e22d
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-20 12:14:02 -0800

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,11 @@
+const stats = document.getElementById('stats');
 
+const clicks = JSON.parse(localStorage.getItem('clicks')) || {};
+
+let output = '<h3>Project Clicks</h3>';
+
+for (let project in clicks) {
+  output += `<p>${project}: ${clicks[project]}</p>`;
+}
+
+stats.innerHTML = output;

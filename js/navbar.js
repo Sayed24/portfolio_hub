ee7cfc7fec1db7d2e515fdcdce35d4adc7435b30
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-21 21:44:12 -0800

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,1 +1,15 @@
+const nav = document.querySelector(".navbar");
 
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const current = window.scrollY;
+
+  if (current > lastScroll && current > 100) {
+    nav.style.transform = "translateY(-100%)";
+  } else {
+    nav.style.transform = "translateY(0)";
+  }
+
+  lastScroll = current;
+});

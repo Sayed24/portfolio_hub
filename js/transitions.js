62074bbe7e36e2fa43e09f8373024ddd4b5ce90f
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-21 21:39:39 -0800

    Update transitions.js

--- js/transitions.js
+++ js/transitions.js
@@ -1,1 +1,15 @@
+const overlay = document.getElementById("pageTransition");
 
+document.querySelectorAll(".nav-link").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+
+    overlay.classList.add("active");
+
+    setTimeout(() => {
+      window.location.href = href;
+    }, 400);
+  });
+});

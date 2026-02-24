e28ce6b7cc2dbb9883d0ab66d1f0001d6d1475b7
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-23 16:26:11 -0800

    Update skills.js

--- js/skills.js
+++ js/skills.js
@@ -1,1 +1,13 @@
+const ctx=document.getElementById("skillsChart");
 
+new Chart(ctx,{
+  type:"radar",
+  data:{
+    labels:["JavaScript","UI/UX","HTML/CSS","Performance","Accessibility","PWA"],
+    datasets:[{
+      label:"Skill Level",
+      data:[95,90,95,85,80,88]
+    }]
+  },
+  options:{responsive:true}
+});

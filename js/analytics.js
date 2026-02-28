90894362a5645bdf756534cc8c332bdde11b8be0
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-02-28 11:25:38 -0800

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,1 +1,25 @@
 
+const analytics={
+visits: Number(localStorage.getItem("visits")||0)+1,
+projectViews:Number(localStorage.getItem("projectViews")||0)
+};
+
+localStorage.setItem("visits",analytics.visits);
+
+export function trackProject(){
+analytics.projectViews++;
+localStorage.setItem("projectViews",analytics.projectViews);
+}
+
+export function renderAnalytics(){
+const box=document.createElement("div");
+box.className="analytics-box";
+
+box.innerHTML=`
+<h4>Recruiter Analytics</h4>
+<p>Visits: ${analytics.visits}</p>
+<p>Project Views: ${analytics.projectViews}</p>
+`;
+
+document.body.appendChild(box);
+}

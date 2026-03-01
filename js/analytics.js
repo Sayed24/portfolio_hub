54ec1bca19763a155c366c3d52a5f9ca04e19a5f
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-01 13:20:38 -0800

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,25 +1,18 @@
+let projectViews =
+JSON.parse(localStorage.getItem("projectStats") || "{}");
 
-const analytics={
-visits: Number(localStorage.getItem("visits")||0)+1,
-projectViews:Number(localStorage.getItem("projectViews")||0)
-};
-
-localStorage.setItem("visits",analytics.visits);
-
-export function trackProject(){
-analytics.projectViews++;
-localStorage.setItem("projectViews",analytics.projectViews);
+export function trackProject(id){
+projectViews[id]=(projectViews[id]||0)+1;
+localStorage.setItem("projectStats",JSON.stringify(projectViews));
 }
 
-export function renderAnalytics(){
-const box=document.createElement("div");
-box.className="analytics-box";
-
-box.innerHTML=`
-<h4>Recruiter Analytics</h4>
-<p>Visits: ${analytics.visits}</p>
-<p>Project Views: ${analytics.projectViews}</p>
-`;
-
-document.body.appendChild(box);
+export function getTopProject(){
+let max=0, top=null;
+for(const id in projectViews){
+if(projectViews[id]>max){
+max=projectViews[id];
+top=id;
+}
+}
+return top;
 }

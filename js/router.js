f20d60facc9b0ff25c3585e934a2e995e23c20d2
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-01 13:29:01 -0800

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,1 +1,14 @@
+const params=new URLSearchParams(window.location.search);
+const id=params.get("id");
 
+if(id){
+const container=document.getElementById("projectContainer");
+
+if(container){
+container.innerHTML=`
+<h1>Project ${id}</h1>
+<p>Dynamic storytelling project page.</p>
+<img src="https://picsum.photos/1200/700?random=${id}">
+`;
+}
+}

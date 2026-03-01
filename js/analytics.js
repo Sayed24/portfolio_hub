fc8b00de4118894dfae5638ebfe0ca8f3259bbf6
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-01 13:30:10 -0800

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,18 +1,4 @@
-let projectViews =
-JSON.parse(localStorage.getItem("projectStats") || "{}");
-
-export function trackProject(id){
-projectViews[id]=(projectViews[id]||0)+1;
-localStorage.setItem("projectStats",JSON.stringify(projectViews));
-}
-
-export function getTopProject(){
-let max=0, top=null;
-for(const id in projectViews){
-if(projectViews[id]>max){
-max=projectViews[id];
-top=id;
-}
-}
-return top;
-}
+let visits=localStorage.getItem("visits")||0;
+visits++;
+localStorage.setItem("visits",visits);
+console.log("Visits:",visits);

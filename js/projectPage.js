c1c7013253ad7a76c8b2ad2685edd1336d6b404c
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-06 09:59:43 -0800

    Update projectPage.js

--- js/projectPage.js
+++ js/projectPage.js
@@ -1,1 +1,29 @@
 
+export async function renderProject(){
+
+const id=location.pathname.split("/project/")[1]
+
+const res=await fetch("/data/projects.json")
+const projects=await res.json()
+
+const p=projects.find(x=>x.id===id)
+
+const app=document.getElementById("app")
+
+app.innerHTML=`
+
+<div class="projectPage">
+
+<img src="${p.image}">
+
+<h1>${p.title}</h1>
+
+<p>${p.description}</p>
+
+<a href="${p.link}" target="_blank">Open Project</a>
+
+</div>
+
+`
+
+}

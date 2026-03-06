44d390da848beac53f29af710e2ed75db0e4a4e2
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-05 20:47:37 -0800

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,15 @@
 
+export function initSearch(projects, render) {
+  const input = document.getElementById("searchInput");
+
+  input.addEventListener("input", e => {
+    const term = e.target.value.toLowerCase();
+
+    const filtered = projects.filter(p =>
+      p.title.toLowerCase().includes(term) ||
+      p.tags.join(" ").includes(term)
+    );
+
+    render(filtered);
+  });
+}

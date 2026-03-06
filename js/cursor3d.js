99311e6a38fcb2c8521d34cdcafd284a6f3009b9
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-06 10:01:29 -0800

    Update cursor3d.js

--- js/cursor3d.js
+++ js/cursor3d.js
@@ -1,1 +1,13 @@
 
+export function initCursor(){
+
+const cursor=document.getElementById("cursor3d")
+
+document.addEventListener("mousemove",e=>{
+
+cursor.style.transform=
+`translate3d(${e.clientX}px,${e.clientY}px,0)`
+
+})
+
+}

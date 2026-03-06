9b51c6326ccb1ce90a58f548fc297e1ff16ff87e
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-06 10:02:19 -0800

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,42 @@
+export function initAdmin(){
 
+const app=document.getElementById("app")
+
+app.innerHTML=`
+
+<h2>Add Project</h2>
+
+<input id="title" placeholder="Title">
+
+<input id="image" placeholder="Image URL">
+
+<textarea id="desc"></textarea>
+
+<button id="save">Save</button>
+
+`
+
+document.getElementById("save").onclick=()=>{
+
+const title=document.getElementById("title").value
+const image=document.getElementById("image").value
+const desc=document.getElementById("desc").value
+
+const newProject={
+id:title.toLowerCase().replaceAll(" ","-"),
+title,
+image,
+description:desc
+}
+
+let projects=JSON.parse(localStorage.getItem("projects"))||[]
+
+projects.push(newProject)
+
+localStorage.setItem("projects",JSON.stringify(projects))
+
+alert("Saved locally")
+
+}
+
+}

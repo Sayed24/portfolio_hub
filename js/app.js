5e18fd65a3ed11168d8c0d2890fd3f788d733f00
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-07 05:30:20 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,32 +1,24 @@
 import * as router from "./router.js"
 import {renderProjects} from "./projects.js"
-import {initSearch} from "./search.js"
-import {initCursor} from "./cursor3d.js"
+import {renderProject} from "./projectPage.js"
+import {initHero3D} from "./hero3d.js"
+import {initCursor} from "./cursor.js"
 import {initHeatmap} from "./heatmap.js"
-import {initInterview} from "./interviewAI.js"
+import {renderAnalytics} from "./analytics.js"
+import {initChatbot} from "./chatbot.js"
 import {initAdmin} from "./admin.js"
-import {optimize} from "./performance.js"
 
 window.router=router
 
 router.addRoute("/",renderProjects)
-router.addRoute("/interview",initInterview)
+router.addRoute("/analytics",renderAnalytics)
+router.addRoute("/chat",initChatbot)
 router.addRoute("/admin",initAdmin)
-
-document.addEventListener("DOMContentLoaded",()=>{
 
 router.render()
 
-initSearch()
+initHero3D()
 
 initCursor()
 
 initHeatmap()
-
-optimize()
-
-if("serviceWorker" in navigator){
-navigator.serviceWorker.register("/sw.js")
-}
-
-})

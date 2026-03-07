91d186f8bd4b69c442341e77f1348aae5507c361
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-07 05:33:33 -0800

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,23 @@
-let visits=localStorage.getItem("visits")||0;
-visits++;
-localStorage.setItem("visits",visits);
-console.log("Visits:",visits);
+export function renderAnalytics(){
+
+const app=document.getElementById("app")
+
+let visits=Number(localStorage.getItem("visits")||0)
+
+visits++
+
+localStorage.setItem("visits",visits)
+
+app.innerHTML=`
+
+<div class="vision-panel">
+
+<h2>Portfolio Analytics</h2>
+
+<p>Total visits: ${visits}</p>
+
+</div>
+
+`
+
+}

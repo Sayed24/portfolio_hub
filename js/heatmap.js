7978828af6e3c0b91d76ad3577574783e709e387
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-07 05:33:18 -0800

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -1,7 +1,6 @@
-
 export function initHeatmap(){
 
-const canvas=document.getElementById("heatmapCanvas")
+const canvas=document.getElementById("heatmap")
 
 const ctx=canvas.getContext("2d")
 
@@ -14,7 +13,7 @@ ctx.beginPath()
 
 ctx.arc(e.clientX,e.clientY,20,0,Math.PI*2)
 
-ctx.fillStyle="rgba(255,0,0,0.05)"
+ctx.fillStyle="rgba(255,0,0,.05)"
 
 ctx.fill()
 

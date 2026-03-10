e5fe659a9344b83b501b04034d398584fd5f9fda
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-10 10:13:26 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 import * as router from "./router.js"
 import {renderProjects} from "./projects.js"
-import {renderProject} from "./projectPage.js"
-import {initHero3D} from "./hero3d.js"
-import {initCursor} from "./cursor.js"
-import {initHeatmap} from "./heatmap.js"
 import {renderAnalytics} from "./analytics.js"
 import {initChatbot} from "./chatbot.js"
 import {initAdmin} from "./admin.js"
+import {initHero3D} from "./hero3d.js"
+import {initCursor} from "./cursor.js"
+import {initHeatmap} from "./heatmap.js"
 
 window.router=router
 

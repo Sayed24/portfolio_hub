6eed26afd1ba6be850137e8fecde00ea98f532db
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-12 07:05:36 -0700

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,1 +1,39 @@
 
+export async function renderProjects(){
+
+const res=await fetch("data/projects.json")
+const projects=await res.json()
+
+const app=document.getElementById("app")
+
+app.innerHTML=`<div class="projects-grid"></div>`
+
+const grid=document.querySelector(".projects-grid")
+
+projects.forEach(project=>{
+
+const card=document.createElement("div")
+card.className="card"
+
+card.innerHTML=`
+
+<img src="${project.image}">
+
+<div class="card-content">
+
+<h3>${project.title}</h3>
+
+<p>${project.description}</p>
+
+</div>
+`
+
+card.onclick=()=>{
+router.navigate("/project/"+project.id)
+}
+
+grid.appendChild(card)
+
+})
+
+}

f4f3ff8f01dd7d6e70e2bef760613d4245b903bd
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-12 07:04:25 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 
+import * as router from "./router.js"
+import {renderProjects} from "./projects.js"
+import {renderProject} from "./projectPage.js"
+import {initTheme} from "./theme.js"
+
+window.router=router
+
+router.addRoute("/",renderProjects)
+router.addRoute("/projects",renderProjects)
+
+router.render()
+
+initTheme()

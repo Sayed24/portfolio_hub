8c7524fe76bc0a6f71da8fd8dd53aaab307644b5
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-12 07:05:09 -0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,1 +1,27 @@
 
+const routes={}
+
+export function addRoute(path,view){
+routes[path]=view
+}
+
+export function navigate(path){
+history.pushState({},'',path)
+render()
+}
+
+export function render(){
+
+const path=location.pathname
+
+if(routes[path]){
+routes[path]()
+}
+
+else if(path.startsWith("/project/")){
+import("./projectPage.js").then(m=>m.renderProject())
+}
+
+}
+
+window.onpopstate=render

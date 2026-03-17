0cc933bd728b1909813da8d41714349233f51854
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-17 09:59:36 -0700

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,1 +1,20 @@
 
+export function initSearch(){
+
+const input=document.getElementById("searchInput")
+
+input.addEventListener("input",()=>{
+
+const term=input.value.toLowerCase()
+
+document.querySelectorAll(".card").forEach(card=>{
+
+const text=card.innerText.toLowerCase()
+
+card.style.display=text.includes(term)?"block":"none"
+
+})
+
+})
+
+}

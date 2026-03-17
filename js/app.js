37a9d1ab1b9177b9e967f2e22d510247be368470
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-17 09:58:23 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 import * as router from "./router.js"
 import {renderProjects} from "./projects.js"
 import {renderProject} from "./projectPage.js"
+import {renderSkills} from "./skills.js"
 import {initTheme} from "./theme.js"
+import {initAdmin} from "./admin.js"
 
 window.router=router
 
 router.addRoute("/",renderProjects)
 router.addRoute("/projects",renderProjects)
+router.addRoute("/about",renderSkills)
+router.addRoute("/admin",initAdmin)
 
 router.render()
 
 initTheme()
+
+if("serviceWorker" in navigator){
+navigator.serviceWorker.register("/sw.js")
+}

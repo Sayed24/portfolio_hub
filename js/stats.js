c226d0ee44014f1ebb5f8494fdd854e8090de11e
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-18 09:40:44 -0700

    Update stats.js

--- js/stats.js
+++ js/stats.js
@@ -1,1 +1,18 @@
 
+export function updateViews(id){
+
+let stats=JSON.parse(localStorage.getItem("views")||"{}")
+
+stats[id]=(stats[id]||0)+1
+
+localStorage.setItem("views",JSON.stringify(stats))
+
+}
+
+export function getViews(id){
+
+let stats=JSON.parse(localStorage.getItem("views")||"{}")
+
+return stats[id]||0
+
+}

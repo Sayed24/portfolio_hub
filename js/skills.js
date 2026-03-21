9765da2148fbb4141f6541a5878250b3b357183a
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-20 23:25:02 -0700

    Update skills.js

--- js/skills.js
+++ js/skills.js
@@ -1,33 +1,14 @@
 export function renderSkills(){
 
-const app=document.getElementById("app")
-
-app.innerHTML=`
-
-<div style="max-width:600px;margin:auto">
-
-<h2>Skills</h2>
-
-<canvas id="skillsChart"></canvas>
-
-</div>
-
+document.getElementById("app").innerHTML=`
+<canvas id="chart"></canvas>
 `
 
-const ctx=document.getElementById("skillsChart")
-
-new Chart(ctx,{
+new Chart(document.getElementById("chart"),{
 type:"bar",
 data:{
-labels:["HTML","CSS","JavaScript","UI Design","Dashboards"],
-datasets:[{
-data:[95,92,90,85,88],
-backgroundColor:"#0071e3"
-}]
-},
-options:{
-animation:{duration:1500}
+labels:["HTML","CSS","JS"],
+datasets:[{data:[95,90,92]}]
 }
 })
-
 }

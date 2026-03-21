c0e84d02815aa6893301ea7b62c6b50714a07684
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-20 23:24:50 -0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,30 +1,8 @@
-
 export function initAdmin(){
-
-const app=document.getElementById("app")
-
-app.innerHTML=`
-
+document.getElementById("app").innerHTML=`
 <div style="padding:40px">
-
-<h2>Add Project</h2>
-
-<input id="title" placeholder="Title"><br><br>
-<input id="image" placeholder="Image URL"><br><br>
-<input id="link" placeholder="Project URL"><br><br>
-
-<textarea id="desc" placeholder="Description"></textarea><br><br>
-
-<button id="save">Save</button>
-
+<h2>Admin Panel</h2>
+<p>Static demo (GitHub Pages limitation)</p>
 </div>
-
 `
-
-document.getElementById("save").onclick=()=>{
-
-alert("Static GitHub Pages cannot save files, but this panel shows how it would work.")
-
-}
-
 }

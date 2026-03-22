efcdf5a497b7559fcb3028dbf2839132f51f0c1f
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-21 19:39:48 -0700

    Update projectPage.js

--- js/projectPage.js
+++ js/projectPage.js
@@ -4,31 +4,20 @@ export async function renderProject(){
 
 const id=location.pathname.split("/project/")[1]
 
-const res=await fetch("data/projects.json")
-const projects=await res.json()
+const res=await fetch("/data/projects.json")
+const data=await res.json()
 
-const p=projects.find(x=>x.id===id)
-
-const app=document.getElementById("app")
+const p=data.find(x=>x.id===id)
 
 const views=getViews(id)
 
-app.innerHTML=`
-
-<div class="page" style="padding:40px">
-
+document.getElementById("app").innerHTML=`
+<div style="padding:40px">
 <h1>${p.title}</h1>
-
-<img src="${p.image}" style="max-width:600px;width:100%">
-
+<img src="https://api.microlink.io/?url=${p.link}&screenshot=true">
 <p>${p.description}</p>
-
 <p><b>Views:</b> ${views}</p>
-
 <a href="${p.link}" target="_blank">Open Project</a>
-
 </div>
-
 `
-
 }

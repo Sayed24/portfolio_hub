44aa8ff94334eef731ec2bd3f0ef83bba0f24345
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-21 19:40:25 -0700

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,1 +1,10 @@
 
+window.filterTag=(tag)=>{
+document.querySelectorAll(".card").forEach(card=>{
+if(tag==="all"||card.dataset.tags.includes(tag)){
+card.style.display="block"
+}else{
+card.style.display="none"
+}
+})
+}

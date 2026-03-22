03499e7b551ef377582b45e5e4d01bdebe761bdf
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-22 06:59:10 -0700

    Delete js/admin.js

--- js/admin.js
+++
@@ -1,8 +0,0 @@
-export function initAdmin(){
-document.getElementById("app").innerHTML=`
-<div style="padding:40px">
-<h2>Admin Panel</h2>
-<p>Static demo (GitHub Pages limitation)</p>
-</div>
-`
-}

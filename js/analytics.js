7961977ae51392e0cbf8796d118951c6791baff5
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-23 11:08:16 -0700

    Update analytics.js

--- js/analytics.js
+++ js/analytics.js
@@ -1,10 +1,5 @@
-export function updateViews(id){
-let v=JSON.parse(localStorage.getItem("views")||"{}")
-v[id]=(v[id]||0)+1
-localStorage.setItem("views",JSON.stringify(v))
-}
-
-export function getViews(id){
-let v=JSON.parse(localStorage.getItem("views")||"{}")
-return v[id]||0
+function trackView(id) {
+  let views = JSON.parse(localStorage.getItem("views")) || {};
+  views[id] = (views[id] || 0) + 1;
+  localStorage.setItem("views", JSON.stringify(views));
 }

8bc587260380cfcf5e5957408d762ac169d94ad3
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-24 06:52:12 -0700

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,1 +1,16 @@
 
+function navigateToProject(id) {
+  history.pushState({ id }, "", `?project=${id}`);
+  renderProjectPage(id);
+}
+
+window.addEventListener("popstate", () => {
+  const params = new URLSearchParams(window.location.search);
+  const id = params.get("project");
+
+  if (id) {
+    renderProjectPage(id);
+  } else {
+    renderProjects(projects);
+  }
+});

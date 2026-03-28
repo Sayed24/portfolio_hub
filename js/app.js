a1eeba29211331b6add18a7e5ada8b22cd84fc49
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-03-27 22:01:21 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -102,6 +102,15 @@ document.getElementById("chatInput").addEventListener("keydown", e => {
     e.target.value = "";
   }
 });
+document.addEventListener("mousemove", e => {
+  document.querySelectorAll("button").forEach(btn => {
+    const rect = btn.getBoundingClientRect();
 
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+
+    btn.style.transform = `translate(${x*0.1}px, ${y*0.1}px)`;
+  });
+});
 /* START */
 loadProjects();

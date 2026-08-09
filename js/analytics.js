1fd9e3d22933ddf508d98cfe8852d7801f7ae315
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2026-08-09 10:17:50 -0700

    Delete js directory

--- js/analytics.js
+++
@@ -1,5 +0,0 @@
-function trackView(id) {
-  let views = JSON.parse(localStorage.getItem("views")) || {};
-  views[id] = (views[id] || 0) + 1;
-  localStorage.setItem("views", JSON.stringify(views));
-}
--- js/search.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("search").addEventListener("input", e => {
-  const val = e.target.value.toLowerCase();
-
-  const filtered = projects.filter(p =>
-    p.title.toLowerCase().includes(val)
-  );
-
-  renderProjects(filtered);
-});
